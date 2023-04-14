0cde4f5a9aaeb0c533908409b2841da832c9fe2e
Joey <98641142+Hangghost@users.noreply.github.com>
2023-04-14 06:18:08 +0000

    Separate load to SQL and Download

--- webapp/home.py
+++ webapp/home.py
@@ -10,38 +10,52 @@ home = Blueprint("home", __name__)
 def download_youtube_video():
   # 訪問的時候是GET
   # 送出表個的時候是POST
+
   
   if request.method == "POST":
-    # 把用戶在前端的網址取出來
-    video_url = request.form.get("downloadUrl")
+    action = request.form.get('action')
+    if action == "loadtoSQL":
+      # 把用戶在前端的網址取出來
+      video_url = request.form.get("downloadUrl")
   
-    # 用取出來的網址，創一個新的youtube物件
-    youtube_video_object = YouTube(video_url)
-
-    # 用物件內的功能取得觀看數、作者、影片名稱
-    views = youtube_video_object.views
-    author = youtube_video_object.author
-    video_title = youtube_video_object.title
-
-    # 寫入資料庫
-    new_video = Video(views, author, video_title)
-    db.session.add(new_video)    # session表示新增一個資料庫的執行
-    db.session.commit()    # commit表示確定執行
+      # 用取出來的網址，創一個新的youtube物件
+      youtube_video_object = YouTube(video_url)
   
-    # 下載影片到伺服器
-    get_video = youtube_video_object.streams.get_highest_resolution()
-
-    
-    # 讓用戶端下載，選擇存放路徑
-    return send_file(get_video.download(), as_attachment=True)
-    #redirect(url_for("home.download_youtube_video"))
+      # 用物件內的功能取得觀看數、作者、影片名稱
+      views = youtube_video_object.views
+      author = youtube_video_object.author
+      video_title = youtube_video_object.title
   
+      # 寫入資料庫
+      new_video = Video(views, author, video_title)
+      db.session.add(new_video)  # session表示新增一個資料庫的執行
+      db.session.commit()  # commit表示確定執行
+  
+      # 下載影片到伺服器
+      global get_video
+      get_video = youtube_video_object.streams.get_highest_resolution()
+  
+      videos = Video.query.all()  # 在Video物件中查詢
+      # 讓用戶端下載，選擇存放路徑
+      return render_template("home.html", videos=videos)
+
+    elif action == "download":
+      if get_video is None:
+        return "尚未存取影片。請先將影片存取到資料庫中"
+      else:
+        return send_file(get_video.download(), as_attachment=True)
+
+    else:
+      pass
+      
+
   # 如果不是POST就執行GET
   else:
     # 讀取資料庫
-    videos = Video.query.all()    # 在Video物件中查詢
-    
+    videos = Video.query.all()  # 在Video物件中查詢
+
     return render_template("home.html", videos=videos)
+
 
 # 刪除功能
 @home.route("/delete/<int:id>", methods=["POST"])
@@ -59,5 +73,5 @@ def delete_video(id):
 
 
 if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+  db.create_all()
+  app.run(debug=True)
